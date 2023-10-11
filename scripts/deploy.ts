a19b4c1505319b69b540d1ecba39ff21654116db
Divyang Chauhan <chauhan.divyang@gmail.com>
2023-10-11 17:00:15 +0530

    added heir to deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,7 +1,23 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const inheritance = await ethers.deployContract("Inheritance");
+  const [deployer] = await ethers.getSigners();
+
+  // Make sure to edit this and add address of heir
+  const heir = "";
+
+  if (heir === "") {
+    throw new Error("Please add heir address to scripts/deploy.ts");
+  }
+
+  console.log("\n Deploying contracts with the account:", deployer.address);
+
+  console.log(
+    "\n Account balance:",
+    (await deployer.provider.getBalance(deployer.address)).toString()
+  );
+
+  const inheritance = await ethers.deployContract("Inheritance", [heir]);
 
   await inheritance.waitForDeployment();
 
