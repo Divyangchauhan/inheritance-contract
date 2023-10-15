7c481a2d822717657340638539d55a49492e41fb
Divyang Chauhan <chauhan.divyang@gmail.com>
2023-10-15 15:30:49 +0530

    moved heir address to env file

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,13 +1,15 @@
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
 import { ethers } from "hardhat";
 
 async function main() {
   const [deployer] = await ethers.getSigners();
 
   // Make sure to edit this and add address of heir
-  const heir = "";
+  const heir = process.env.HeirAddress;
 
-  if (heir === "") {
-    throw new Error("Please add heir address to scripts/deploy.ts");
+  if (!heir) {
+    throw new Error("Please add HeirAddress to .env file");
   }
 
   console.log("\n Deploying contracts with the account:", deployer.address);
